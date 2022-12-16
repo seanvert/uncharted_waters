12494c6083aea77f63002885feecbc80cfdb7ffc
Sean Uchida <seankuchida@gmail.com>
2022-12-16 09:45:00 -0300

    roadmap

--- app/src/main.rs
+++ app/src/main.rs
@@ -8,86 +8,84 @@ extern crate piston;
 // extern crate gfx;
 
 pub mod app {
-    pub use view::view::render;
     pub use model::model;
-	pub use sprites::sprites;
+    pub use sprites::sprites;
+    pub use view::view::render;
     // use gfx_device_gl::{Resources, CommandBuffer};
     use piston::event_loop::{EventSettings, Events};
     //use piston::input::Input::Move;
     //use piston::ButtonState;
-	use crate::piston::{
-		ResizeEvent, FocusEvent, MouseCursorEvent, ButtonEvent,
-		UpdateEvent, RenderEvent, Window, EventLoop
-	};
+    use crate::piston::{
+        ButtonEvent, EventLoop, FocusEvent, MouseCursorEvent, RenderEvent, ResizeEvent,
+        UpdateEvent, Window,
+    };
     use piston::window::WindowSettings;
+    use piston_window::OpenGL;
     use piston_window::PistonWindow;
-	use piston_window::OpenGL;
     // use graphics::*;
-	// use graphics::rectangle::square;
-
+    // use graphics::rectangle::square;
 
     pub struct App {
-		pub model: model::Model,
+        pub model: model::Model,
     }
 
     impl App {
-		pub fn new() -> App {
-			App {
-				model: model::Model::new(),
-			}
-		}
+        pub fn new() -> App {
+            App {
+                model: model::Model::new(),
+            }
+        }
 
-		pub fn run(&mut self) {
-			let opengl = OpenGL::V3_2;
-			let mut window: PistonWindow = WindowSettings::new("Uncharted Waters", [1366, 768])
-				.graphics_api(opengl)
-				.exit_on_esc(true)
-				.resizable(true)
-				.fullscreen(true)
-				.build()
-				.unwrap();
-			window.set_max_fps(30);
-			let mut events = Events::new(EventSettings::new());
-			sprites::on_load(&mut window, &mut self.model.player);
-			println!("{} {}", window.size().width, window.size().height);
-			// TODO colocar uma rotina para criar um menu
-			// TODO opção para carregar o jogo
+        pub fn run(&mut self) {
+            let opengl = OpenGL::V3_2;
+            let mut window: PistonWindow = WindowSettings::new("Uncharted Waters", [1366, 768])
+                .graphics_api(opengl)
+                .exit_on_esc(true)
+                .resizable(true)
+                .fullscreen(true)
+                .build()
+                .unwrap();
+            window.set_max_fps(30);
+            let mut events = Events::new(EventSettings::new());
+            sprites::on_load(&mut window, &mut self.model.player);
+            println!("{} {}", window.size().width, window.size().height);
+            // TODO colocar uma rotina para criar um menu
+            // TODO opção para carregar o jogo
 
-			while let Some(mut e) = events.next(&mut window) {
-				if let Some(args) = e.render_args() {
-					// TODO ajeitar o path disso
-					// FIXME passar um iterável com a lista de objetos
-					// atualmente está passando só um objeto
-					// app vai precisar conter um iterável com os
-					// objetos que serão renderizados na cena
-					// FIXME colocar o obj no model depois e passar
-					// pra cá
-					view::view::render(&mut self.model, &args,
-											  &mut window, &mut e);
-				}
-				if let Some(args) = e.update_args() {
-					model::update(&mut self.model, &args);
-				}
-				if let Some(inp) = e.button_args() {
-					self.model.controls.on_input(piston::Input::Button(inp));
-				}
-				if let Some(coordinates) = e.mouse_cursor_args() {
-					self.model.controls.mouse_input(coordinates);
-				}
-				if let Some(focus) = e.focus_args() {
-					println!("{:?}", focus);
-				}
-				if let Some(args) = e.resize_args() {
-					self.model.window_size = args.window_size;
-					println!("{:?}", args);
-				}
-			}
-		}
+            while let Some(mut e) = events.next(&mut window) {
+                if let Some(args) = e.render_args() {
+                    // TODO ajeitar o path disso
+                    // FIXME passar um iterável com a lista de objetos
+                    // atualmente está passando só um objeto
+                    // app vai precisar conter um iterável com os
+                    // objetos que serão renderizados na cena
+                    // FIXME colocar o obj no model depois e passar
+                    // pra cá
+                    view::view::render(&mut self.model, &args, &mut window, &mut e);
+                }
+                if let Some(args) = e.update_args() {
+                    model::update(&mut self.model, &args);
+                }
+                if let Some(inp) = e.button_args() {
+                    self.model.controls.on_input(piston::Input::Button(inp));
+                }
+                if let Some(coordinates) = e.mouse_cursor_args() {
+                    self.model.controls.mouse_input(coordinates);
+                }
+                if let Some(focus) = e.focus_args() {
+                    println!("{:?}", focus);
+                }
+                if let Some(args) = e.resize_args() {
+                    self.model.window_size = args.window_size;
+                    println!("{:?}", args);
+                }
+            }
+        }
     }
 }
 
 use crate::app::App;
-fn main () {
-	let mut jogo = crate::App::new();
-	jogo.run();
+fn main() {
+    let mut jogo = crate::App::new();
+    jogo.run();
 }
--- object/src/lib.rs
+++ object/src/lib.rs
@@ -1,60 +1,61 @@
 pub mod object {
-// TODO: acho que o certo seria encapsular a sprite num tipo próprio dela
-    use gfx_graphics::Texture;
+    // TODO: acho que o certo seria encapsular a sprite num tipo próprio dela
     use gfx_device_gl::Resources;
+    use gfx_graphics::Texture;
 
-	pub struct Object {
-		pub rot: f64,
-		pub x: f64,
-		pub y: f64,
-		pub sprite: Option<Texture<Resources>>
-	}
+    pub struct Object {
+        pub rot: f64,
+        pub x: f64,
+        pub y: f64,
+        pub sprite: Option<Texture<Resources>>,
+    }
 
-	impl Object {
-		pub fn new() -> Object {
-			Object {
-				rot: 0.0,
-				x: 0.0, y: 0.0,
-				sprite: None
-			}
-		}
-		// TODO: acho que deveria trocar isso para um tipo cena
-		pub fn new_vec() -> Vec<Object> {
-			let mut objects: Vec<Object> = Vec::with_capacity(30);
-			for _i in 0..29 {
-				objects.push(Object::new());
-			}
-			objects
-		}
-		pub fn get_rot(&mut self) -> f64{
-			return self.rot;
-		}
-		
-		pub fn rot(&mut self, r: f64) {
-			self.rot += r;
-		}
+    impl Object {
+        pub fn new() -> Object {
+            Object {
+                rot: 0.0,
+                x: 0.0,
+                y: 0.0,
+                sprite: None,
+            }
+        }
+        // TODO: acho que deveria trocar isso para um tipo cena
+        pub fn new_vec() -> Vec<Object> {
+            let mut objects: Vec<Object> = Vec::with_capacity(30);
+            for _i in 0..29 {
+                objects.push(Object::new());
+            }
+            objects
+        }
+        pub fn get_rot(&mut self) -> f64 {
+            return self.rot;
+        }
 
-		pub fn rot_to(&mut self, r: f64) {
-			self.rot = r;
-		}
-		
-		pub fn mov(&mut self, x: f64, y: f64) {
-			self.x += x;
-			self.y += y;
-		}
+        pub fn rot(&mut self, r: f64) {
+            self.rot += r;
+        }
 
-		pub fn fwd(&mut self, d: f64) {
-			self.x += d * (-self.rot.sin());
-			self.y += d * self.rot.cos();
-		}
+        pub fn rot_to(&mut self, r: f64) {
+            self.rot = r;
+        }
 
-		pub fn mov_to(&mut self, x: f64, y: f64) {
-			self.x = x;
-			self.y = y;
-		}
+        pub fn mov(&mut self, x: f64, y: f64) {
+            self.x += x;
+            self.y += y;
+        }
 
-		pub fn set_sprite(&mut self, sprite: Texture<Resources>) {
-			self.sprite = Some(sprite);
-		}
-	}
+        pub fn fwd(&mut self, d: f64) {
+            self.x += d * (-self.rot.sin());
+            self.y += d * self.rot.cos();
+        }
+
+        pub fn mov_to(&mut self, x: f64, y: f64) {
+            self.x = x;
+            self.y = y;
+        }
+
+        pub fn set_sprite(&mut self, sprite: Texture<Resources>) {
+            self.sprite = Some(sprite);
+        }
+    }
 }
